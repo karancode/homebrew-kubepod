207df36f88597729efe0e4b86ef0fb12142a1faf
Karan Thanvi <karan.thanvi@rakuten.com>
2019-04-24 19:22:56 +0900

    fixes for homebrew/core

--- kubepod.rb
+++ kubepod.rb
@@ -1,23 +1,19 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Kubepod < Formula
   desc "List/Search pods faster in kubectl"
   homepage "https://github.com/kdthanvi/kubepod"
   url "https://github.com/kdthanvi/kubepod/archive/v1.0.0.tar.gz"
   sha256 "84dbe84e83d9d1445631978f54f93c3d9d592e518b2d52848413696674cb56e2"
-  # depends_on "cmake" => :build
 
   bottle :unneeded
- 
-  option "with-short-names", "link as \"kpod\" instead"
-  depends_on "kubernetes-cli" => :recommended
+
+  depends_on "grep"
+  depends_on "kubernetes-cli"
 
   def install
-	bin.install "kubepod"  => build.with?("short-names") ? "kpod" : "kubepod"
+    bin.install "kubepod"  => build.with?("short-names") ? "kpod" : "kubepod"
   end
 
   test do
-	assert_match "USAGE:", shell_output("#{bin}/kubectx -h 2>&1", 1)  
+    assert_match "USAGE:", shell_output("#{bin}/kubepod -h 2>&1")
   end
 end
